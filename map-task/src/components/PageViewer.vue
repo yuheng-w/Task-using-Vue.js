130dfe7a1ac12bebf1b916f33ecf7e5dbfe35324
Yuheng <50189364+yuheng-w@users.noreply.github.com>
2023-06-11 14:33:41 -0400

    add universal time

--- map-task/src/components/PageViewer.vue
+++ map-task/src/components/PageViewer.vue
@@ -1,11 +1,28 @@
 <template>
     <div id="content" class="container">
         <h1> {{ pageTitle }}</h1>
+        <div> Current time: {{ time }} </div>
     </div>
 </template>
 
 <script>
 export default {
     props: ['pageTitle'],
+
+    data () {
+        return {
+            time: ""
+        };
+    },
+
+    mounted() {
+        setInterval(() => {
+            let date = new Date();
+            let utcHours = date.getUTCHours();
+            let utcMinutes = date.getUTCMinutes();
+            let utcSeconds = date.getUTCSeconds();
+            this.time = 'hours: ' + utcHours + ', minites:' + utcMinutes + ', seconds:' +  utcSeconds;
+        }, 1000);
+    }
 }
 </script>
