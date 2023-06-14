715b8159907766502b7cfed6a1349a2a5e7c2814
Yuheng <50189364+yuheng-w@users.noreply.github.com>
2023-06-13 20:31:33 -0400

    add pagination

--- map-task/src/components/SearchedRecords.vue
+++ map-task/src/components/SearchedRecords.vue
@@ -1,11 +1,19 @@
 <template>
     <div>
         <ul>
-            <li v-for="(record, index) in records" :key="index">
+            <li v-for="(record, index) in displayedItems" :key="index">
                 <SingleRecord :record="record"></SingleRecord>
-                <div v-if="index === 0"> {{ timezone }} <br> {{ localTime }}</div>
+                <div v-if="start + index === 0"> {{ timezone }} <br> {{ localTime }}</div>
             </li>
         </ul>
+        <button @click="previousPage" :disabled="currentPage === 1">Previous</button>
+        <button @click="nextPage" :disabled="currentPage === totalPages">Next</button>
+        
+        <div>
+            <span v-for="pageNumber in totalPages" :key="pageNumber">
+                <button @click="goToPage(pageNumber)">{{ pageNumber }}</button>
+            </span>
+        </div>
     </div>
 </template>
 
@@ -19,7 +27,11 @@ export default {
         return {
             apiKey: process.env.VUE_APP_GOOGLE_MAPS_API_KEY,
             timezone: "",
-            localTime: ""
+            localTime: "",
+            currentPage: 1,
+            itemsPerPage: 10,
+            // start: 0,
+            // end: 10
         }
     },
     mounted() {
@@ -45,7 +57,38 @@ export default {
                     this.localTime = localTime;
                 });
             }
+        },
+
+        nextPage() {
+            if (this.currentPage < this.totalPages) {
+            this.currentPage++;
+            }
+        },
+        previousPage() {
+            if (this.currentPage > 1) {
+            this.currentPage--;
+            }
+        },
+        goToPage(pageNumber) {
+            if (pageNumber >= 1 && pageNumber <= this.totalPages) {
+            this.currentPage = pageNumber;
+            }
         }
-    }
+    },
+
+    computed: {
+        start() {
+            return (this.currentPage - 1) * this.itemsPerPage;
+        },
+        end() {
+            return this.start + this.itemsPerPage;
+        },
+        displayedItems() {
+            return this.records.slice(this.start, this.end);
+        },
+        totalPages() {
+            return Math.ceil(this.records.length / this.itemsPerPage);
+        }
+    },
 }
 </script>
